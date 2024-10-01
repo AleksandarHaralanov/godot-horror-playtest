6ec9ae07b9a93c3b587fedc4fb9ee051feb619fb
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-10-01 21:40:54 +0300

    Add head bob and sway, optimize flashlight

--- scripts/player/flashlight.gd
+++ scripts/player/flashlight.gd
@@ -1,21 +1,19 @@
 extends SpotLight3D
 
-
-var toggle: bool = false
 var target_energy: float = 0.0
 
 @onready var toggle_off = $ToggleOff
 @onready var toggle_on = $ToggleOn
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("toggle_flashlight"):
-		if toggle:
+	if Input.is_action_just_pressed("flashlight"):
+		if get_meta("toggle"):
 			target_energy = 0.0
 			toggle_off.play()
 		else:
 			target_energy = 4.0
 			toggle_on.play()
-		toggle = !toggle
+		set_meta("toggle", !get_meta("toggle"))
 
 func _process(delta: float) -> void:
 	light_energy = lerp(light_energy, target_energy, delta * 10)
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -9,6 +9,12 @@ const CROUCHING_SPEED = 2.5
 const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
 	if Input.is_action_pressed("sprint") and !Input.is_action_pressed("crouch"):
 		current_speed = SPRINTING_SPEED
 	elif Input.is_action_pressed("crouch") and !Input.is_action_pressed("sprint"):
@@ -16,19 +22,14 @@ func _physics_process(delta: float) -> void:
 	else:
 		current_speed = WALKING_SPEED
 	
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * current_speed
-		velocity.z = direction.z * current_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, current_speed)
-		velocity.z = move_toward(velocity.z, 0, current_speed)
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * current_speed
+			velocity.z = direction.z * current_speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, current_speed)
+			velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
