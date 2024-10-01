3fb139ce1aaf9bdf036a741eaf772a5276910d6e
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-10-01 16:48:05 +0300

    Organize folders and files, add world environment, rename input maps, add flashlight lerp and sound fx

--- scripts/player/flashlight.gd
+++ scripts/player/flashlight.gd
@@ -1,6 +1,21 @@
 extends SpotLight3D
 
 
+var toggle: bool = false
+var target_energy: float = 0.0
+
+@onready var toggle_off = $ToggleOff
+@onready var toggle_on = $ToggleOn
+
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("flashlight"):
-		visible = !visible
+	if Input.is_action_just_pressed("toggle_flashlight"):
+		if toggle:
+			target_energy = 0.0
+			toggle_off.play()
+		else:
+			target_energy = 4.0
+			toggle_on.play()
+		toggle = !toggle
+
+func _process(delta: float) -> void:
+	light_energy = lerp(light_energy, target_energy, delta * 10)
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -22,7 +22,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * current_speed
