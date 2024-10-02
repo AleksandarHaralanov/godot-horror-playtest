8c18c7d471ddcdf26f33dc84657f65547a058122
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-10-02 21:23:24 +0300

    Encapsulate scripts

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -11,11 +11,14 @@ const CAMERA_ACCELERATION: float = 5.0
 var current_speed: float = 0.0
 var head_y_axis: float = 0.0
 var camera_x_axis: float = 0.0
+var flashlight_target_energy: float = 0.0
 
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera
 @onready var hand: Node3D = $Hand
 @onready var flashlight: SpotLight3D = $Hand/Flashlight
+@onready var flashlight_toggle_off: AudioStreamPlayer3D = $Hand/Flashlight/ToggleOff
+@onready var flashlight_toggle_on: AudioStreamPlayer3D = $Hand/Flashlight/ToggleOn
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -24,27 +27,51 @@ func _input(event) -> void:
 	if event is InputEventMouseMotion:
 		head_y_axis += event.relative.x * CAMERA_SENSITIVITY
 		camera_x_axis -= event.relative.y * CAMERA_SENSITIVITY
-		
 		camera_x_axis = clamp(camera_x_axis, -90, 90)
 
+	if Input.is_action_just_pressed("flashlight"):
+		if flashlight.get_meta("toggle"):
+			flashlight_target_energy = 0.0
+			flashlight_toggle_off.play()
+		else:
+			flashlight_target_energy = 4.0
+			flashlight_toggle_on.play()
+		flashlight.set_meta("toggle", !flashlight.get_meta("toggle"))
+
 func _process(delta: float) -> void:
+	_rotations(delta)
+	
+	_controls_and_movement(delta)
+	
+	_flashlight_light(delta)
+	
+	move_and_slide()
+
+func _rotations(delta: float) -> void:
 	head.rotation.y = lerp(head.rotation.y, -deg_to_rad(head_y_axis), CAMERA_ACCELERATION * delta)
 	camera.rotation.x = lerp(camera.rotation.x, deg_to_rad(camera_x_axis), CAMERA_ACCELERATION * delta)
 	
 	hand.rotation.y = -deg_to_rad(head_y_axis)
 	flashlight.rotation.x = deg_to_rad(camera_x_axis)
-	
+
+func _flashlight_light(delta: float) -> void:
+	flashlight.light_energy = lerp(flashlight.light_energy, flashlight_target_energy, delta * 10)
+
+func _get_current_speed() -> float:
+	if Input.is_action_pressed("sprint") and !Input.is_action_pressed("crouch"):
+		return SPRINTING_SPEED
+	elif Input.is_action_pressed("crouch") and !Input.is_action_pressed("sprint"):
+		return CROUCHING_SPEED
+	else:
+		return WALKING_SPEED
+
+func _controls_and_movement(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	else:
 		velocity.y -= GRAVITY * delta
 	
-	if Input.is_action_pressed("sprint") and !Input.is_action_pressed("crouch"):
-		current_speed = SPRINTING_SPEED
-	elif Input.is_action_pressed("crouch") and !Input.is_action_pressed("sprint"):
-		current_speed = CROUCHING_SPEED
-	else:
-		current_speed = WALKING_SPEED
+	current_speed = _get_current_speed()
 	
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -55,5 +82,3 @@ func _process(delta: float) -> void:
 		else:
 			velocity.x = move_toward(velocity.x, 0, current_speed)
 			velocity.z = move_toward(velocity.z, 0, current_speed)
-	
-	move_and_slide()
